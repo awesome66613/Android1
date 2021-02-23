757788f7e5e49dceee2854a7e1b273c59b3cbc48
awesome66613 <nechepurnov.m@gmail.com>
2021-02-23 13:34:23 +0300

    HomeWork1

--- app/src/main/java/com/example/lesson1/MainActivity.java
+++ app/src/main/java/com/example/lesson1/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.lesson1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.textview);
+        EditText editText = findViewById(R.id.edit_text);
+        Button buttom_main = findViewById(R.id.button);
+        Switch switch_main = findViewById(R.id.switch1);
+        CheckBox checkBox = findViewById(R.id.checkBox);
     }
+
 }
